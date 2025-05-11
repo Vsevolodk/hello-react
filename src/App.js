43ea28b678696a6ef3b0a3989a8707c59d62477d
Vsevolod <vs_konst@inbox.ru>
2025-05-11 16:01:45 +0400

    Первый коммит

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="container">
+        <h1>Привет, React!</h1>
+        <p>Это простое приложение, работающее и на компьютере, и на смартфоне.</p>
+      </div>
   );
 }
 
